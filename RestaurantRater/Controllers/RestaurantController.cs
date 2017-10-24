cb34e8449143006bca6e86cf2cd7bca78d3571fa
digitopoi <hubo85@gmail.com>
2017-10-23 19:33:59 -0500

    Add delete functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using RestaurantRater.Models;
 using System.Net;
+using System.Data.Entity;
 
 namespace RestaurantRater.Controllers
 {
@@ -76,6 +77,39 @@ namespace RestaurantRater.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        //  GET: Restaurant/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+
+        //  POST: Restaurant/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RestaurantID,Name,Address,Rating")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(restaurant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
